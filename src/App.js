f3abf66ffb279b769b0e35c1cc8ee7be8f6ac825
Praveen Ramachandran <praveen.thayikkattil@gmail.com>
2020-08-08 04:03:02 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import moment from "moment";
+import React, { Component } from "react";
+import logo from "./clockface.png";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalClocks: 1,
+      currentLocalTime: moment.utc(moment()).local(),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(
+      () => this.setState({ currentLocalTime: moment.utc(moment()).local() }),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    let matchTimeFormatted = this.state.currentLocalTime.format(
+      "hh:mm:ss | LTS"
+    );
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="app-logo-wrap">
+            <div className="clock-outer-ring"></div>
+            <div className="clock-inner-ring"></div>
+
+            <img src={logo} className="App-logo" alt="logo" />
+
+            <div className="clock-needles-wrap">
+              <div className="second-hand-needle"></div>
+              <div className="minute-hand-needle"></div>
+              <div className="hour-hand-needle"></div>
+            </div>
+          </div>
+
+          <p>
+            An <code>RTC Clock App</code> with ability to add multiple clocks
+            for various timezones.
+          </p>
+
+          <p>{matchTimeFormatted}</p>
+
+          <p>
+            Add New RTC Clock for a different time zone, for example canada :D
+          </p>
+
+          <button href="instagram.com" className="App-action-button">
+            Add New RTC Clock
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
